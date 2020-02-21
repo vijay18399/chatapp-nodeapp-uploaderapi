3cc3fe449a04b33d731e4cb6dcd54c6a5a0d2eec
vijay18399 <vijayreddy18399@gmail.com>
2020-02-21 20:03:32 +0530

    added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fileupload = require('express-fileupload');
 var app = express();
 var mime = require('mime-types')
 var port = process.env.PORT || 3000;
+var path = require('path');
 app.use(cors());
 app.use(fileupload());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,22 +17,54 @@ app.post("/upload", function(req, res) {
   const original = file.name;
   const mimetype= file.mimetype;
   console.log(file,original,mimetype);
-  file.name  = mime.extension(file.mimetype)+new Date().valueOf()+'.'+mime.extension(file.mimetype);
+  const ext = path.extname(original).slice(1);
+  file.name  = new Date().valueOf()+'.'+ext;
+  const  image = ['jpg', 'png', 'gif', 'jpeg'];
+  const video = ['mp4', 'mov', 'mpg', 'flv','mkv','avi','3gp'];
+  const audio =['mp3','m4a','wav'];
+  const lang=['cpp','txt','c','java','py','go','cs','r','html','css','rb','perl','sql','js','ts'];
+  const compress=['zip','rar','7z'];
+  const documents=['ppt','doc','pptx','docx','pdf'];
+  const pc =['exe','msi'];
+
+  if(image.includes(ext)){
+  type = 'image';
+  }
+  else if(video.includes(ext)){
+    type = 'video';
+  } else if(audio.includes(ext)){
+    type = 'audio';
+  } else if(compress.includes(ext)){
+    type = 'compress';
+  }   else if(documents.includes(ext)){
+    type = 'documents';
+  }  else if(pc.includes(ext)){
+    type = 'pc';
+  } else if(lang.includes(ext)){
+    type = 'lang';
+  } else if(ext == 'apk'){
+    type = 'android'
+  }else{
+    type = ext
+  }
+
+
   file.mv('./uploads/' + file.name, function(err, result) {
    if(err) 
 {
   res.send({
     err,
     success: false,
-    message: "File failed!",
+    message: "File upload  failed!",
    });
 }
    res.send({
-     ext:mime.extension(file.mimetype),
+     ext:ext,
     success: true,
     message: "File uploaded!",
     file:file.name,
-    original:original
+    original:original,
+    type:type
    });
   })
  })
